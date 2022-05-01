1c0282b988b1a28df6da6e0be9e6f629d6d02432
yue <kqiyue@126.com>
2022-05-01 20:37:44 +0800

    lab02

--- lab02.py
+++ lab02.py
@@ -107,6 +107,47 @@ def count_cond(condition):
     return fun_return
 
 
+'''Q7: Composite Identity Function'''
+def composer(f, g):
+    """Return the composition function which given x, computes f(g(x)).
+
+    >>> add_one = lambda x: x + 1        # adds one to x
+    >>> square = lambda x: x**2
+    >>> a1 = composer(square, add_one)   # (x + 1)^2
+    >>> a1(4)
+    25
+    >>> mul_three = lambda x: x * 3      # multiplies 3 to x
+    >>> a2 = composer(mul_three, a1)    # ((x + 1)^2) * 3
+    >>> a2(4)
+    75
+    >>> a2(5)
+    108
+    """
+    return lambda x: f(g(x))
+
+def composite_identity(f, g):
+    """
+    Return a function with one parameter x that returns True if f(g(x)) is
+    equal to g(f(x)). You can assume the result of g(x) is a valid input for f
+    and vice versa.
+
+    >>> add_one = lambda x: x + 1        # adds one to x
+    >>> square = lambda x: x**2
+    >>> b1 = composite_identity(square, add_one)
+    >>> b1(0)                            # (0 + 1)^2 == 0^2 + 1
+    True
+    >>> b1(4)                            # (4 + 1)^2 != 4^2 + 1
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    def fun(n):
+        a = composer(f, g)
+        b = composer(g, f)
+        return a(n) == b(n)
+    return fun
+
+
+
 
 
 
