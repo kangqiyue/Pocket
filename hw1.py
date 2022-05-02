731fc6feb5d19b6ea95c435462eda2adc045b883
yue <kqiyue@126.com>
2022-05-02 23:41:15 +0800

    hw01

---
+++ hw1.py
@@ -0,0 +1,83 @@
+'''Q2: k in Num'''
+def k_in_num(k, num):
+    """
+    Complete k_in_num, a function which returns True if num has the digit k and
+    returns False if num does not have the digit k. 0 is considered to have no
+    digits.
+
+    >>> k_in_num(3, 123) # .Case 1
+    True
+    >>> k_in_num(2, 123) # .Case 2
+    True
+    >>> k_in_num(5, 123) # .Case 3
+    False
+    >>> k_in_num(0, 0) # .Case 4
+    False
+    """
+    "*** YOUR CODE HERE ***"
+
+    while num%10 != k and num >0:
+        num = num//10
+
+    if num == 0:
+        return False
+    else:
+        return True
+
+
+'''Q3: A Plus Abs B'''
+from operator import add, sub
+def a_plus_abs_b(a, b):
+    """Return a+abs(b), but without calling abs.
+
+    >>> a_plus_abs_b(2, 3)
+    5
+    >>> a_plus_abs_b(2, -3)
+    5
+    >>> a_plus_abs_b(-1, 4)
+    3
+    >>> a_plus_abs_b(-1, -4)
+    3
+    """
+    if b < 0:
+        f = sub
+    else:
+        f = add
+    return f(a, b)
+
+
+'''Q4: Two of Three'''
+def two_of_three(i, j, k):
+    """Return m*m + n*n, where m and n are the two smallest members of the
+    positive numbers i, j, and k.
+
+    >>> two_of_three(1, 2, 3)
+    5
+    >>> two_of_three(5, 3, 1)
+    10
+    >>> two_of_three(10, 2, 8)
+    68
+    >>> two_of_three(5, 5, 5)
+    50
+    """
+    return i**2 + j**2 + k **2 - max(i,j,k)**2
+
+'''Q5: Largest Factor'''
+def largest_factor(n):
+    """Return the largest factor of n that is smaller than n.
+
+    >>> largest_factor(15) # factors are 1, 3, 5
+    5
+    >>> largest_factor(80) # factors are 1, 2, 4, 5, 8, 10, 16, 20, 40
+    40
+    >>> largest_factor(13) # factor is 1 since 13 is prime
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    i = 1
+    factors = []
+    while i<n:
+        if n%i == 0:
+            factors.append(i)
+        i+=1
+    return max(factors)
